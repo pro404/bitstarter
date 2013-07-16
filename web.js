b0ed08d5488187c29acefb44baf0cf875f42e72a
pro404 <umarsaeed@outlook.com>
2013-07-16 12:33:38 -0700

    added index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var buf = new Buffer(5000);
+var infile = "index.html";
+
+buf = fs.readFileSync(infile);
+var out = buf.toString('utf-8',0,buf.length);
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
